1dff185924953e87efdb7e8b6ca328da3cdca661
Muhammad Hamza <hamza35502@gmail.com>
2025-11-23 23:08:11 +0500

    Initial Setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name = "Medical Chatbot",
+    version = "0.1.0",
+    author = "Muhammad Hamza",
+    author_email = "hamza35502@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
